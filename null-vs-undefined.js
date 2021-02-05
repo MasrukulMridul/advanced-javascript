e080912c8ece6bd5f8466fe212fe2caffe1c9521
MasrukulMridul <masrukulmridul15@gmail.com>
2021-02-05 10:38:12 +0600

    nul vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,24 @@
+let pakhi;
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return      //return likhlam ,but bole delam nah ki return korbo tokhon undefined dekhabe.
+}
+
+
+function add(num1, num2){
+    console.log(num1, num2);
+        
+    }
+
+
+
+const premik = {name:"smart boy", phone:01521438561};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+
+let ages = [2, 9, 8];
+console.log(ages[11]);
--- true-false.js
+++ true-false.js
@@ -1,3 +1,15 @@
+//false
+//0
+// ""
+//undefined
+//null
+//NaN
+
+//true
+//"0" cotetion er vhitor string thakle
+//"  " cotetion er vhitor gap thakle o 
+
+
 const name = 'Salman khan';
 if(name.length>0){
     console.log("condition is true")
