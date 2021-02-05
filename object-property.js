34f87f2a82483a78c47e444af3192b3247c2e359
MasrukulMridul <masrukulmridul15@gmail.com>
2021-02-05 14:11:58 +0600

    object property

--- object-property.js
+++ object-property.js
@@ -10,4 +10,4 @@ const ids = students.map(s => s.id);
 const bigger = students.filter( s => s.id>40);
 const biggerOne = students.find( s => s.id>40);
 
-console.log(biggerOne);
+console.log(names);
